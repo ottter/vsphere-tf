6e33020e869b64d6ddeda8cbbe176966e546b06d
James <29265756+ottter@users.noreply.github.com>
2023-05-02 16:18:43 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 }
 provider "vsphere" {
-  user        = var.vsphere_user
+  user         = var.vsphere_user
   password     = var.vsphere_password
   vsphere_server       = var.vsphere_vcenter
   allow_unverified_ssl = true
